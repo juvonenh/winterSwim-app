0684e473db587f83d9ba7925fe7a1b3f3ee1ac2d
Hanna Juvonen <hanna.juvonen@myy.haaga-helia.fi>
2024-11-10 22:47:00 +0200

    Lisatty karttapohja

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+
 export default function Map() {
+  // Create a state for region
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+  });
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Map Screen</Text>
-    </View>
+    // Use region prop in the MapView and the value comes from region state
+    <MapView style={{ width: "100%", height: "100%" }} region={region}>
+      <Marker coordinate={region} />
+    </MapView>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>Map Screen</Text>
+    // </View>
   );
 }
